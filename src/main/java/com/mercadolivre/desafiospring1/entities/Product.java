a109f501b7574a37909d2066c1134df6df230d38
Gabriel Amurim De Souza <gabriel.gasouza@mercadolivre.com>
2021-12-28 15:18:57 -0300

    Product entitie and json database

---
+++ src/main/java/com/mercadolivre/desafiospring1/entities/Product.java
@@ -0,0 +1,22 @@
+package com.mercadolivre.desafiospring1.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    private Long productId;
+    private String name;
+    private String category;
+    private String brand;
+    private BigDecimal price;
+    private boolean freeShipping;
+    private String prestige;
+
+}
