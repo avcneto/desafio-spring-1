5427a03094f90e4d16e91e244c07429e592aa75f
nathaliadantasv <nathalia.dantas@mercadolivre.com>
2021-12-29 10:33:30 -0300

    created getProductByAnyQuery

--- src/main/java/com/mercadolivre/desafiospring1/controllers/ProductController.java
+++ src/main/java/com/mercadolivre/desafiospring1/controllers/ProductController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RequestMapping("/api/v1/articles")
 @RestController
@@ -19,14 +20,8 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping()
-    public ResponseEntity<List<Product>> getProduct(){
-        List<Product> productList = productService.findAllProducts();
-    return ResponseEntity.ok(productList);
-    }
-
-    @GetMapping(params = "category")
-    public ResponseEntity<List<Product>> getProductByCategory(@RequestParam("category") String categoryName) {
-        List<Product> productList = productService.findProductsByCategory(categoryName);
+    public ResponseEntity<List<Product>> getProductByAnyQuery(@RequestParam Map<String, String> requestParams) {
+        List<Product> productList = productService.getProductByAnyQuery(requestParams);
         return ResponseEntity.ok(productList);
     }
 }
--- src/main/java/com/mercadolivre/desafiospring1/services/ProductService.java
+++ src/main/java/com/mercadolivre/desafiospring1/services/ProductService.java
@@ -3,11 +3,15 @@ package com.mercadolivre.desafiospring1.services;
 import com.mercadolivre.desafiospring1.entities.Product;
 import com.mercadolivre.desafiospring1.exception.RepositoryException;
 import com.mercadolivre.desafiospring1.repositories.ProductRepository;
+import com.mercadolivre.desafiospring1.util.Is;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -26,8 +30,34 @@ public class ProductService {
         return products;
     }
 
-    public List<Product> findProductsByCategory(String category){
-        List<Product> products = findAllProducts();
-        return products.stream().filter(product -> product.getCategory().equalsIgnoreCase(category)).collect(Collectors.toList());
+    public List<Product> getProductByAnyQuery(Map<String, String> requestParams) {
+
+        List<Product> productList = findAllProducts();
+
+        for (Map.Entry<String, String> entry : requestParams.entrySet()) {
+            if (!entry.getValue().equals(null)) {
+                productList = productList.stream().filter(product -> {
+                    String valor = requestParams.get(entry.getKey());
+
+                    if(Is.isNumericException(valor) && product.getPrice().compareTo(new BigDecimal(String.valueOf(valor))) <= 0)
+                        return true;
+                    if(Is.isBooleanException(valor) && product.isFreeShipping() == Boolean.parseBoolean(valor))
+                        return true;
+                    if (product.getCategory().equalsIgnoreCase(valor))
+                        return true;
+                    if (product.getBrand().equalsIgnoreCase(valor))
+                        return true;
+                    if (product.getName().equalsIgnoreCase(valor))
+                        return true;
+                    if (product.getPrestige().equalsIgnoreCase(valor))
+                        return true;
+
+                    return false;
+
+                }).collect(Collectors.toList());
+            }
+        }
+
+        return productList;
     }
 }
